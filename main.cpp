3c543eda61adb753d4671b09c7cfea0f8946202f
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-05-24 19:17:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main () {
+    int n;
+    cin>>n;
+    float max[n];
+    for (int i=0; i<n; i++) {
+        cin>>max[i];
+    }
+    float m=n_max(max, n);
+    cout<<m;
+
+    return 0;
 }
