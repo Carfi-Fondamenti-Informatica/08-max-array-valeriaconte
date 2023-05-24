fc3d17dbf9164659f246c2523247e0398d11833c
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-05-24 19:18:39 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float n_max (float max[], int a){
+    float m=max[0];
+    for (int i=0; i<a; i++) {
+        if(max[i]>m){
+            m=max[i];
+        }
+    }
+    return m;
+}
